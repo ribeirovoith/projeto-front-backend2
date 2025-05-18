72f7265acd70df5b8d42ee3eb90a873669fc640e
Pedro Henrique <pedro.sribeiro@aluno.faculdadeimpacta.com.br>
2025-05-18 20:35:19 -0300

    Define rota raiz com mensagem motivacional

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'frontend')));
-
-app.get('/hello', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Esteja sempre investindo em você, de forma pessoal e profissional.');
 });
 
